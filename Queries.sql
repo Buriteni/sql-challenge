cdfdab71e81ca5da0f214c748d77d49ff8d9dc4e
Brittney Hill <britthillre@gmail.com>
2023-09-02 22:46:32 -0500

    New

--- Queries.sql
+++ Queries.sql
@@ -1,10 +1,12 @@
 select e.emp_no, last_name, first_name, sex, salary
 from employees as e join salaries as s 
 on e.emp_no = s.emp_no
+limit 20
 
 select first_name, last_name, hire_date
 from employees 
 where hire_date between '1986-01-01' and '1986-12-31'
+limit 20
 
 select dep.dept_no, dept_name, emp.emp_no, last_name, first_name
 from dept_manager as mang join departments as dep
@@ -20,6 +22,7 @@ on dep.emp_no = emp.emp_no
 join dept_manager as mang
 on mang.dept_no = dep.dept_no
 
+
 select first_name, last_name, sex
 from employees
 where first_name = 'Hercules' 
@@ -33,6 +36,7 @@ on dep.emp_no = emp.emp_no
 join dept_manager as mang
 on mang.dept_no = dep.dept_no
 where dept_name = 'Sales'
+limit 20
 
 select dept_name, emp.emp_no, last_name, first_name
 from dept_emp as dep join employees as emp
@@ -43,10 +47,10 @@ join dept_manager as mang
 on mang.dept_no = dep.dept_no
 where dept_name = 'Sales'
 or dept_name = 'Development'
-
+limit 20
 
 select last_name, count(last_name)
 from employees
 group by last_name
 order by count(last_name) desc
-
+limit 20
